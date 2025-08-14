8f73d25982eaf9a88f9470859248c11a0b8a3810
SJDSF DEV <sjdsfdeveloper@sjdsf.lk>
2025-08-14 13:30:34 +0530

    added nullable fields in card interface

--- web-app/src/types/card.ts
+++ web-app/src/types/card.ts
@@ -2,10 +2,10 @@ export interface Card {
   student_id: number;
   first_name: string;
   last_name: string;
-  employer_name: string;
-  check_in_date_time: Date;
-  check_out_date_time: Date;
-  check_in_time: string;
-  check_out_time: string;
-  emotion: string;
+  employer_name: string | null;
+  check_in_date_time: Date | null;
+  check_out_date_time: Date | null;
+  check_in_time: string | null;
+  check_out_time: string | null;
+  emotion: string | null;
 }
