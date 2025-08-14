429ad0810c57849535850678600ed35a5d9f44f9
SJDSF DEV <sjdsfdeveloper@sjdsf.lk>
2025-08-14 13:38:24 +0530

    set check_in_date_time to string instead of Date

--- web-app/src/types/card.ts
+++ web-app/src/types/card.ts
@@ -3,8 +3,8 @@ export interface Card {
   first_name: string;
   last_name: string;
   employer_name: string | null;
-  check_in_date_time: Date | null;
-  check_out_date_time: Date | null;
+  check_in_date_time: string | null;
+  check_out_date_time: string | null;
   check_in_time: string | null;
   check_out_time: string | null;
   emotion: string | null;
