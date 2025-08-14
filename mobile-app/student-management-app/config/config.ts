aef5f48fa5dec534a2835ad799873e85f6fd235b
SJDSF DEV <sjdsfdeveloper@sjdsf.lk>
2025-08-14 11:18:01 +0530

    gunicorn package added

--- mobile-app/student-management-app/config/config.ts
+++ mobile-app/student-management-app/config/config.ts
@@ -1,2 +1,2 @@
 export const BASE_URL =
-  "https://87e89eab-95e5-4c0f-8192-7ee0196e1581-prod.e1-us-east-azure.choreoapis.dev/employee-mgmt-system/backend/v1.0";
+  "https://87e89eab-95e5-4c0f-8192-7ee0196e1581-prod.e1-us-east-azure.choreoapis.dev/student-management-system/backend/v1.0";
